9e1278493463bde90c86b9999c96cd5784635e66
Adnaan-Alvi-au26 <alviadnan389@gmail.com>
2023-07-05 17:04:06 +0530

    final commit

--- src/Store/CartProvider.js
+++ src/Store/CartProvider.js
@@ -12,6 +12,10 @@ const cartReducer = (state, action) => {
 if (action.type === 'ADD') {
   const updatedTotalAmount =
     state.totalAmount + action.item.price * action.item.amount;
+    // console.log(state.totalAmount + ' state.totalAmount')
+    // console.log(action.item.price +' action.item.price')
+    // console.log(action.item.amount +' action.item.amount')
+    // console.log(updatedTotalAmount +' updated toatlamount')
 
   const existingCartItemIndex = state.items.findIndex(
     (item) => item.id === action.item.id
@@ -35,9 +39,26 @@ if (action.type === 'ADD') {
     totalAmount: updatedTotalAmount,
   };
 }
-//   if (action.type === "REMOVE") {
-//     return;
-//   }
+if (action.type === 'REMOVE') {
+  const existingCartItemIndex = state.items.findIndex(
+    (item) => item.id === action.id
+  );
+  const existingItem = state.items[existingCartItemIndex];
+  const updatedTotalAmount = state.totalAmount - existingItem.price;
+  let updatedItems;
+  if (existingItem.amount === 1) {
+    updatedItems = state.items.filter(item => item.id !== action.id);
+  } else {
+    const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+    updatedItems = [...state.items];
+    updatedItems[existingCartItemIndex] = updatedItem;
+  }
+
+  return {
+    items: updatedItems,
+    totalAmount: updatedTotalAmount
+  };
+}
   return defaultCartState;
 };
 
